c1f6880490c1d2a2bdc2bbe21c56121616975d4b
Klayveer <silvaklayveer@gmail.com>
2024-03-20 17:08:32 -0300

    Atualização do Front/React

---
+++ FrontEnd/cashhub/src/App.tsx
@@ -0,0 +1,11 @@
+Aqui vai a parte importante!
+
+import XXXXX from "./components/xxxxxx"
+
+export default function App(){
+    return (
+        <main>
+            <XXXXX />
+        </main>
+    );
+}
