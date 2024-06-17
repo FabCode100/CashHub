f5570b7e1f4885b254e2ef443de94b98d408f26a
RyanFonsec <ryanfonsecamagalhaes@gmail.com>
2024-06-17 15:29:36 -0300

    organização

--- FrontEnd/cashhub/src/app/index.tsx
+++ FrontEnd/cashhub/src/app/index.tsx
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from './src/screens/LoginScreen';
+import LoginScreen from '../src/screens/LoginScreen';  // Caminho atualizado
 
 const Stack = createStackNavigator();
 
