179fe7384f3722b1e7727305d0c5b2cc448cb578
FabCode100 <fabriciobc47@gmail.com>
2024-06-26 20:51:07 -0300

    Alterações no backend para realizar a integração com sucesso com o frontend

--- BackEnd/cashhub/src/main/java/com/cashub/cashhubbackend/cashub/controller/PaymentController.java
+++ BackEnd/cashhub/src/main/java/com/cashub/cashhubbackend/cashub/controller/PaymentController.java
@@ -5,6 +5,7 @@ import com.cashub.cashhubbackend.cashub.domain.payment.exception.PaymentExceptio
 import com.cashub.cashhubbackend.cashub.dto.PaymentRequest;
 import com.cashub.cashhubbackend.cashub.dto.PaymentResponse;
 import com.cashub.cashhubbackend.cashub.service.PaymentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -28,12 +29,29 @@ public class PaymentController {
 
     @PostMapping("/process")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public ResponseEntity<PaymentResponse> processPayment(@RequestBody PaymentRequest request) throws PaymentException {
-        PaymentResponse response = paymentService.processPayment(request);
-        if (response.isSuccess()) {
-            return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.badRequest().body(response);
+    public ResponseEntity<String> processPayment(@RequestBody PaymentRequest request) {
+        // Verifique se os campos obrigatórios não são nulos
+        if (request.cardNumber() == null || request.cardNumber().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Número do cartão é obrigatório");
+        }
+        if (request.expirationDate() == null || request.expirationDate().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Data de expiração é obrigatória");
+        }
+        if (request.cvv() == null || request.cvv().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("CVV é obrigatório");
+        }
+        if (request.amount() == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Valor é obrigatório");
+        }
+
+        // Processe o pagamento aqui
+        try {
+            paymentService.processPayment(request);
+            ResponseEntity.status(HttpStatus.valueOf(200));
+            return ResponseEntity.ok("Pagamento realizado com sucesso");
+        } catch (Exception e) {
+            e.printStackTrace(); // Registre o erro para depuração
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro desconhecido: " + e.getMessage());
         }
     }
 }
